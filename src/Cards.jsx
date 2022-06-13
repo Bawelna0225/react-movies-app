7772d99fc03ebe41f2a77963b536472427967b8c
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-06-13 19:31:12 +0200

    movie details modal

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -2,49 +2,37 @@ import { render } from '@testing-library/react'
 import react, { useState } from 'react'
 import { useEffect } from 'react'
 
-const Card = (movie) => {
-	const [open, setOpen] = useState(false)
-	const handleShowOverview = () => {
-		setOpen(true)
-	}
-	const handleCloseOverview = () => {
-		setOpen(false)
-	}
+const Card = ({movie, updateModal}) => {
 	const handleCardClick = (e) => {
-		console.log(e.target.parentNode.querySelector('img').src)
-		console.log(e.target.parentNode.querySelector('.title'))
-		console.log(e.target.parentNode.querySelector('.rating'))
-		console.log(e.target.parentNode.querySelector('.overview p'))
+		updateModal(movie.title, movie.vote_average, movie.poster_path, movie.overview)
 	}
 	let img_path = 'https://image.tmdb.org/t/p/w500'
 	return (
 		<div>
 			<div className="movie-card"  onClick={handleCardClick}>
-				<img src={img_path + movie.info.poster_path} className="poster"></img>
+				<img src={img_path + movie.poster_path} className="poster"></img>
 				<button className="show-more">
-					<span onClick={handleShowOverview} class="material-symbols-outlined">
+					<span class="material-symbols-outlined">
 						more_horiz
 					</span>
 				</button>
 				<div className="movie-details">
 					<div className="box">
-						<h4 className="title">{movie.info.title}</h4>
+						<h4 className="title">{movie.title}</h4>
 						<div
 							className="rating"
 							style={{
-								background: `conic-gradient(${movie.info.vote_average > 7 ? 'green' : movie.info.vote_average < 3 ? 'red' : 'yellow'} ${movie.info.vote_average * 10}%, #444 ${movie.info.vote_average * 10}%)`,
+								background: `conic-gradient(${movie.vote_average > 7 ? 'green' : movie.vote_average < 3 ? 'red' : 'yellow'} ${movie.vote_average * 10}%, #444 ${movie.vote_average * 10}%)`,
 							}}
 						>
-							<span className="rating-value">{movie.info.vote_average}</span>
+							<span className="rating-value">{movie.vote_average}</span>
 						</div>
 					</div>
 				</div>
-				<div className={open ? 'overview open' : 'overview'}>
-					<p>{movie.info.overview}</p>
-					<button className="close" onClick={handleCloseOverview}>
-						Close
-					</button>
-				</div>
+				{/* <div className={open ? 'overview open' : 'overview'}>
+					<p>{movie.overview}</p> 
+					 <p>{movie.id}</p>
+				</div> */}
 			</div>
 		</div>
 	)
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom/client';
+
 import Cards from './Cards'
 const API_key = '&api_key=84743ee32095533fcd630a3079c2d3f0',
 	base_url = 'https://api.themoviedb.org/3'
 let url = `${base_url}/discover/movie?sort_by=popularity.desc${API_key}`
 let arr = ['Popular', 'Sci-Fi', 'Fantasy', 'Drama', 'Comedy', 'Animation', 'Horror']
+
 const Main = () => {
 	const [movieData, setData] = useState([])
 	const [url_set, setUrl] = useState(url)
 	const [search, setSearch] = useState()
 	const [sideNavOpen, setSideNavOpen] = useState(false)
-
+	const [modalData, setModalData] = useState({})
 	useEffect(() => {
 		fetch(url_set)
 			.then((res) => res.json())
@@ -54,11 +57,60 @@ const Main = () => {
 		setSideNavOpen(!sideNavOpen)
 	}
 
+	const [isModalOpen, setModalOpen] = useState(false)
 
+	const handleCloseModal = () => {
+		setModalOpen(false)
+	}
+	// ---------------------------------------------------------------
+	const Modal = (modalData) => {
+		return (
+			<div className={isModalOpen ? 'modal open' : 'modal'}>
+				<div className="modal-box">
+					<div className="modal-header">
+						<button onClick={handleCloseModal}>
+							<span class="material-symbols-outlined">close</span>
+						</button>
+					</div>
+					<div className="modal-body">
+						<div className="left-side">
+							<div className="poster">
+								<img src={`https://image.tmdb.org/t/p/w500/${modalData.poster_path}`} alt="" />
+							</div>
+						</div>
+						<div className="right-side">
+							<h2 className="title">{modalData.title}</h2>
+							<div className="user-score">
+								<div
+									className="rating"
+									style={{
+										background: `conic-gradient(${modalData.vote_average > 7 ? 'green' : modalData.vote_average < 3 ? 'red' : 'yellow'} ${modalData.vote_average * 10}%, #444 ${modalData.vote_average * 10}%)`,
+									}}
+								>
+									<span className="rating-value">{modalData.vote_average}</span>
+								</div>
+							<p>User Score</p>	
+							</div>
+
+							<div className="overview">
+								<h3>Overview</h3>
+								<p>{modalData.overview}</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+	// ---------------------------------------------------------------------
+	const updateModal = (title, vote_average, poster_path, overview) => {
+		setModalData({title, vote_average, poster_path, overview})
+		setModalOpen(true)
+	}
 	return (
 		<>
 			<div className="header">
-        <div className='logo'>MOVIE APP</div>
+				<div className="logo">MOVIE APP</div>
 				<nav>
 					<div className={sideNavOpen ? 'wrapper-menu open' : 'wrapper-menu'} id="toggle-menu" onClick={toggleSideMenu}>
 						<div className="line-menu half start"></div>
@@ -119,13 +171,14 @@ const Main = () => {
 					</div>
 				</form>
 			</div>
-
+			<div id="modal-here"></div>
+			<Modal {...modalData}/>
 			<div className="container">
 				{movieData.length == 0 ? (
 					<p className="notfound">Not Found</p>
 				) : (
 					movieData.map((res, pos) => {
-						return <Cards info={res} key={pos} />
+						return <Cards movie={res} key={pos} updateModal={updateModal} />
 					})
 				)}
 			</div>
